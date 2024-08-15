c4de59748f3e3eada885ef960ea16ab8b6697e07
bumbleGOD <juanjosetm.22@gmail.com>
2024-08-15 08:23:09 -0500

    cambios

--- Navigation.js
+++ Navigation.js
@@ -2,9 +2,7 @@ import { View, Text } from "react-native";
 import React from "react";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { NavigationContainer } from "@react-navigation/native";
-import Ionicons from '@expo/vector-icons/Ionicons';
-import MaterialIcons from "@expo/vector-icons/MaterialIcons";
-import FontAwesome from '@expo/vector-icons/FontAwesome';
+import AntDesign from '@expo/vector-icons/AntDesign';
 import { createStackNavigator } from "@react-navigation/stack";
 
 import index from "./screens";
@@ -33,7 +31,7 @@ function MyTabs() {
             options={{
                tabBarLabel: () => null,
                tabBarIcon: ({ color, size }) => (
-                  <Ionicons name="home-sharp" size={24} color="black" />
+                  <AntDesign name="user" size={24} color="black" />
                ),
                tabBarBadge: 3,
                headerShown: false,
@@ -45,7 +43,7 @@ function MyTabs() {
             options={{
                tabBarLabel: () => null,
                tabBarIcon: ({ color, size }) => (
-                  <MaterialIcons name="work" size={24} color="black" />
+                  <AntDesign name="adduser" size={24} color="black" />
                ),
                tabBarBadge: 1,
                headerShown: false,
